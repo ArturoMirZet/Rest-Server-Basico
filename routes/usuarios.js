a822c4c2a7724c8a38f3ceafd2fb42a9d084b6f2
Arturo Miranda Zetina <arturo.kjw@gmail.com>
2022-07-11 19:20:11 -0500

    Primer commit

---
+++ routes/usuarios.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const bodyParser = require("body-parser");
+const {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+} = require("../controllers/usuarios");
+
+const router = Router();
+const jsonParser = bodyParser.json();
+
+router.get("/", usuariosGet);
+router.put("/:id", usuariosPut);
+router.post("/", jsonParser, usuariosPost);
+router.delete("/", usuariosDelete);
+router.patch("/", usuariosPatch);
+
+module.exports = router;
